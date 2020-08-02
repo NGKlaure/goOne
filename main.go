8f2227fe289f9bc3bdce18113e5e4bc6dfd7b107
ubuntu <nadinegadjou@gmail.com>
2020-08-02 13:33:48 -0500

    geeting argument with os

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	//fmt.Println("Hello world")
+	var local string = os.Args[1]
+	var greeting string = "hello"
+	if local == "en" {
+		greeting = "hello"
+	} else if local == "fr" {
+		greeting = "bonjour"
+	} else if local == "es" {
+		greeting = "hola"
+	} else if local == "de" {
+		greeting = "gutengtag"
+	} else {
+		greeting = "yo"
+	}
+
+	fmt.Printf(greeting + ",Go!\n")
+
 }
